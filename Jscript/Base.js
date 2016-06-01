384669a1eed18bd981e113f80c2f92e6ec68d005
pxmaomao <987669795@qq.com>
2016-06-01 15:55:10 +0800

    第一次提交

---
+++ Jscript/Base.js
@@ -0,0 +1,26 @@
+﻿
+function HeaderInit() {
+    var s = "";
+    s += "<div class='nav'>";
+    s += "<ul>";
+    s += "<li><a href='../../Home/HomePage.html'>首页</a></li>";
+    s += "<li><a href='../../ChildJquery/home/nav_Home.html'>JQUERY</a></li>";
+    s += "<li><a href='../../ChildHtml5/home/nav_Home.html'>HTML5</a></li>";
+    s += "<li><a href='../../ChildCss3/home/nav_Home.html'>CSS3</a></li>";
+    s += "<li><a href='../../ChildGame/home/nav_Home.html'>小游戏</a></li>";
+    s += "<li><a href='../../ChildChaj/home/nav_Home.html'>插件</a></li>";
+    s += "</ul>";
+    s += "</div>";
+    $(".header").html(s);
+}
+
+function FooterInit() {
+    var s = "";
+    s += "Copyright © Zero";
+    $(".footer").html(s);
+}
+$(function () {
+    HeaderInit();
+    FooterInit();
+})
+
---
+++ Jscript/Base_Child.js
@@ -0,0 +1,26 @@
+﻿function HeaderInit() {
+    var s = "";
+    s += "<span class='span_title'>Zero</span>";
+    $(".header").html(s);
+}
+
+function Top_navInit() {
+    var s = "";
+    s += "<a class='link_home' href='../../Home/HomePage.html'>首页</a>";
+    s += "<a class='link_home' href='../../ChildJquery/home/nav_Home.html'>JQUERY</a>";
+    s += "<a class='link_home' href='../../ChildHtml5/home/nav_Home.html'>HTML5</a>";
+    s += "<a class='link_home' href='../../ChildCss3/home/nav_Home.html'>CSS3</a>";
+    s += "<a class='link_home' href='../../ChildGame/home/nav_Home.html'>小游戏</a>";
+    s += "<a class='link_home' href='../../ChildChaj/home/nav_Home.html'>插件</a>";
+    $(".top_nav").html(s);
+}
+function FooterInit() {
+    var s = "";
+    s += "Copyright © Zero";
+    $(".footer").html(s);
+}
+$(function () {
+    HeaderInit();
+    Top_navInit();
+    FooterInit();
+})
